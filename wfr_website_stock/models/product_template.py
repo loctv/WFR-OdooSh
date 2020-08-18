ffbb874d50036335a96507e756bd9ad5cdbbffbf
Ali Alkaheli <aal@odoo.com>
2020-08-18 13:35:38 -0700

    FIX Modify field

--- wfr_website_stock/models/product_template.py
+++ wfr_website_stock/models/product_template.py
@@ -9,20 +9,3 @@ class ProductTemplate(models.Model):
         ('always_no_lock',
          'Sell regardless of inventory and show inventory on website'),
     ])
-
-    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, parent_combination=False, only_template=False):
-        combination_info = super(ProductTemplate, self)._get_combination_info(
-            combination=combination, product_id=product_id, add_qty=add_qty, pricelist=pricelist,
-            parent_combination=parent_combination, only_template=only_template)
-
-        if combination_info['product_id']:
-            product = self.env['product.product'].sudo().browse(combination_info['product_id'])
-            website = self.env['website'].get_current_website()
-            combination_info.update({
-                'qty_available': product.with_context(warehouse=website.warehouse_id.id).qty_available,
-            })
-        else:
-            combination_info.update({
-                'qty_available': 0,
-            })
-        return combination_info
