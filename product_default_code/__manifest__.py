88508ba554cb3f3054cec7a1741194f663a74395
hibahodges <37086962+hibahodges@users.noreply.github.com>
2020-11-19 22:45:50 -0700

    Add files via upload

---
+++ product_default_code/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Default Code',
+    'category': 'Website',
+    'summary': 'Product Default Code',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'author': 'Atharva System',
+    'website': 'https://www.atharvasystem.com',
+    'support': 'support@atharvasystem.com',
+    'description': """
+Product Default Code
+----
+    """,
+    'depends': ['atharva_theme_general'],
+    'data': [
+        'views/templates.xml'
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+}
